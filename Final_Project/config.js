018d499d82b3d0217cd6cfe6a5ae1cfabebba90d
summer-h-s <96546063+summer-h-s@users.noreply.github.com>
2024-04-16 16:44:47 -0700

    Update config.js

--- Final_Project/config.js
+++ Final_Project/config.js
@@ -60,7 +60,7 @@ let divChapter3 =
   "<p style='max-width:600px; margin-left:auto; margin-right:auto'>By moving the slider back and forth, we are able to see the distribution of data across the nation, where major cities will have the greatest prevalence for incidents. Interestingly, hotspots of incidents involving Indigenous peoples are focused predominantly West of Ontario, happening to coincide with the RCMP’s main jurisdictions. Furthermore, we can see a distinct rural urban-divide with regards to racialized groups, where locations outside of major cities, especially moving North within the Western provinces, as well as northern Territories, are almost entirely represented by police incidents with Indigenous peoples. </p>";
 
 let divChapter4 =
-  "<h3>Looking Closer: "Those who take us away"</h3>" +
+  "<h3>Looking Closer: Those who take us away</h3>" +
   <img width="100" src="https://live.staticflickr.com/2805/9856368246_9ca90c630d_z.jpg"> +
   
   "<p><i>Figure 3.</i> Tachie Reserve, a Dakelh village in Northern British Columbia. By Samer Muscati, 2012.(https://www.flickr.com/photos/sultan/9856368246/in/photostream/).</p>" +
