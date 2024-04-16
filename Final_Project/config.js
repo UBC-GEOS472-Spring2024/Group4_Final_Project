e58e382e8812b1c0b9cb25b074d718383e498caa
summer-h-s <96546063+summer-h-s@users.noreply.github.com>
2024-04-16 16:39:43 -0700

    Update config.js

--- Final_Project/config.js
+++ Final_Project/config.js
@@ -60,29 +60,25 @@ let divChapter3 =
   "<p style='max-width:600px; margin-left:auto; margin-right:auto'>By moving the slider back and forth, we are able to see the distribution of data across the nation, where major cities will have the greatest prevalence for incidents. Interestingly, hotspots of incidents involving Indigenous peoples are focused predominantly West of Ontario, happening to coincide with the RCMP’s main jurisdictions. Furthermore, we can see a distinct rural urban-divide with regards to racialized groups, where locations outside of major cities, especially moving North within the Western provinces, as well as northern Territories, are almost entirely represented by police incidents with Indigenous peoples. </p>";
 
 let divChapter4 =
-  "<h3><b>Looking Closer</b></h3>" +
-  
-  "<h3>Those who take us away</h3>"+
-  
-  '<div><img width="600" src="https://live.staticflickr.com/2805/9856368246_9ca90c630d_z.jpg" alt="Highway of Tears sign with a red dress near Witset, B.C."><div>' +
+  "<h3>Looking Closer: "Those who take us away"</h3>" +
+  <img width="100" src="https://live.staticflickr.com/2805/9856368246_9ca90c630d_z.jpg"> +
   
   "<p><i>Figure 3.</i> Tachie Reserve, a Dakelh village in Northern British Columbia. By Samer Muscati, 2012.(https://www.flickr.com/photos/sultan/9856368246/in/photostream/).</p>" +
   
   "<p>Tachie Reserve, seen in Figure 3, is a Dakelh village in Northern British Columbia. In Dakelh, the Indigenous language of the community, the word for ‘police’ translates to ‘those who take us away.’ An RCMP report examining the historical role of police in Canada’s residential school system revealed that they were seen less as sources of assistance and more as figures of authority who removed community members from the reserve or made arrests for perceived wrongdoing (Human Rights Watch, 2013).</p>" +
   
-  "<p>This speaks to the experience of those who have been taken away.</p>";
+"<p>This speaks to the experience of those who have been taken away.</p>";
 
 let divChapter5 =
-  "<h3><b>Looking Closer</b></h3>" +
-  
-  "<h3>Police Abuse of Indigenous Women</h3>"+
+  "<h3>Looking Closer: Police Abuse of Indigenous Women</h3>" +
   
   "<p>In Saskatchewan, Human Rights Watch found evidence of a fractured relationship between law enforcement and Indigenous communities.</p>";
 
   "<p>The failure of law enforcement authorities to deal effectively with the problem of missing and murdered indigenous women and girls in Canada is just one element of the dysfunctional relationship between the Canadian police and Indigenous communities (Human Rights Watch, 2013).</p>";
 
 let divChapter6 =
-  "<h3>Municipal Example 3</h3>" + "<p>Insert Sabrina's part</p>";
+  "<h3>Looking Closer: Police Abuse of Indigenous Women</h3>" + 
+  "<p>Insert Sabrina's part</p>";
 
 let footerDiv =
   "<p>Insert Conclusion.</p>" +
@@ -91,9 +87,9 @@ let footerDiv =
   '<h4><a href="https://www.mapbox.com/about/maps/" target="_blank">© Mapbox</a> | <a href="http://www.openstreetmap.org/about/" target="_blank">© OpenStreetMap</a></h4>';
 
 var config = {
-  style: "mapbox://styles/mapbox/dark-v11",
+  style: "mapbox://styles/sabrinaqistina/clv30o4p400dh01rj505kcruk",
   accessToken:
-    "pk.eyJ1Ijoic3VtbWVyLWgtcyIsImEiOiJjbHN3b21reGQydmNlMmxwcHh0OWw4cTgwIn0.MpcHLXkWUYWUOBqfMlCZVw",
+    "pk.eyJ1Ijoic2FicmluYXFpc3RpbmEiLCJhIjoiY2t6ZDdlMmdqMm01NTJ5cHJwemRxeDAwbCJ9.EVjeL8Z69BJYubxiNhTLrg",
   showMarkers: false,
   markerColor: "#3FB1CE",
   theme: "light",
@@ -201,8 +197,8 @@ var config = {
       description: "",
       chapterDiv: divChapter5,
       location: {
-        center: [-107.02948, 54.72062],
-        zoom: 4.21,
+        center: [-101.18722, 55.62027],
+        zoom: 3.97,
         pitch: 0,
         bearing: 0,
       },
