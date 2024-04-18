62b20e06ae2b41534619e63136a5a559cc7e1edd
MartinezTau <128635396+MartinezTau@users.noreply.github.com>
2024-04-17 23:42:52 -0700

    Update config.js

--- Final_Project/config.js
+++ Final_Project/config.js
@@ -123,7 +123,7 @@ let divChapter6 =
 let divChapter7 =
   "<h3>Looking Closer: Resistance to Police Brutality Amongst Indigenous Communities in 2020</h3>" + 
   
-  '<video width="100" controls autoplay>  <source src="https://www.bpmcdn.com/files/nunavut/import/videos/2020_06_MVI_8008.mp4" type="video/mp4"> </video>' +
+  '<video width="300" controls autoplay>  <source src="https://www.bpmcdn.com/files/nunavut/import/videos/2020_06_MVI_8008.mp4" type="video/mp4"> </video>' +
   
   "<p>Here, we focus on a province with the highest percentage of the population with Indigenous identity, Nunavut, and one of their acts of resistance towards police brutality which is protesting.</p>" +
 
