29d1eddd0a6844a8ca309df776b32dcf1c0e38f1
MartinezTau <128635396+MartinezTau@users.noreply.github.com>
2024-04-17 19:03:30 -0700

    Update config.js

--- Final_Project/config.js
+++ Final_Project/config.js
@@ -124,18 +124,7 @@ let divChapter6 =
 
 let divChapter7 =
   "<h3>Looking Closer: Resistance to Police Brutality Amongst Indigenous Communities in 2020</h3>" + 
-
-'<video width="500px" 
-               height="400px" 
-               controls="controls">
-        <source 
-                src=
-"https://www.bpmcdn.com/files/nunavut/import/videos/2020_06_MVI_8008.mp4" 
-                type="video/mp4" />
-       </video>' +
-  
-  //'<video width="500" src="https://www.bpmcdn.com/files/nunavut/import/videos/2020_06_MVI_8008.mp4" >' +
-
+  "<p> video </p>" +
   "<p>Here, we focus on a province with the highest percentage of the population with Indigenous identity, Nunavut, and one of their acts of resistance towards police brutality which is protesting.</p>" +
 
   "<p>On June 5, 2020, hundreds of protesters in Iqaluit gathered to express their frustration with police brutality and racism.</p>";
@@ -362,7 +351,7 @@ var config = {
       callback: "",
       onChapterEnter: [
         {
-          layer: "data./highwayoftears.html",
+          layer: "pviolenceData",
           opacity: 0.8,
           duration: 300,
         },
